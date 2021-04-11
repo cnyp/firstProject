65846f76feb1397d9d3e23a571b3f8582f15d633
cnyp <phuongcao0910@gmail.com>
2021-04-11 17:21:05 +0200

    connection Java Postgres

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,17 +1,55 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class application {
-        public static void main(String args[]) {
-            int [] numbers = {10, 20, 30, 40, 50};
+    public static void main(String args[]) {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "admin");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
 
-            for(int x : numbers ) {
-                System.out.print( x );
-                System.out.print(",");
-            }
-            System.out.print("\n");
-            String [] names = {"James", "Larry", "Tom", "Lacy"};
+            stmt = c.createStatement();
 
-            for( String name : names ) {
-                System.out.print( name );
-                System.out.print(",");
-            }
+
+            String sqlCreate = "DROP TABLE IF EXISTS COMPANY; " +
+                    "CREATE TABLE COMPANY " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sqlCreate);
+
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
         }
+        System.out.println("Records created successfully");
+    }
 }
+
